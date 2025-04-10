aff2213c395e25154d4e82d2ed1aae8f7e029d38
Yubin <qls773322@naver.com>
2025-04-10 21:42:25 +0900

    feat: 중복된 게시물 제한

--- src/main/java/org/sopt/service/PostService.java
+++ src/main/java/org/sopt/service/PostService.java
@@ -2,6 +2,7 @@ package org.sopt.service;
 
 import org.sopt.domain.Post;
 import org.sopt.repository.PostRepository;
+import org.sopt.util.GeneratorId;
 
 import java.util.List;
 
@@ -9,11 +10,11 @@ public class PostService {
 
     // postRepository 가져오기
     private final PostRepository postRepository = new PostRepository();
-    private int postId = 1;
 
     // 게시글 저장
-    public void createPost(String title){
-        Post post = new Post(postId++, title);
+    public void createPost(String title) throws IllegalArgumentException{
+        duplicatePost(title);
+        Post post = new Post(GeneratorId.generateId(), title);
         postRepository.save(post);
     }
 
@@ -33,7 +34,20 @@ public class PostService {
     }
 
     // 게시글 수정
-    public boolean updatePostTitle(int id, String title){
+    public boolean updatePostTitle(int id, String title) throws IllegalArgumentException{
+        duplicatePost(title);
         return postRepository.update(id, title);
+    }
+
+    // 게시글 찾기
+    public List<Post> searchPostsByKeyword(String keyword){
+        return postRepository.search(keyword);
+    }
+
+    // 중복된 게시물
+    private void duplicatePost(String title){
+        if (postRepository.existsPostByTitle(title)){
+            throw new IllegalArgumentException("❌ 중복된 게시물입니다.");
+        }
     }
 }
