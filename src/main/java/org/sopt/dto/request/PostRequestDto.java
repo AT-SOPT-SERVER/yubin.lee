513519b775e1f4e0564c371b1bb28cfac209f730
Yubin <qls773322@naver.com>
2025-04-29 21:12:32 +0900

    feat: image 추가

--- src/main/java/org/sopt/dto/request/PostRequestDto.java
+++ src/main/java/org/sopt/dto/request/PostRequestDto.java
@@ -6,5 +6,7 @@ import jakarta.validation.constraints.Size;
 public record PostRequestDto(
         @NotBlank(message = "제목을 입력해야 합니다.")
         @Size(max = 30, message = "제목은 30자 이하여야 합니다.")
-        String title
+        String title,
+        String imageName,
+        String imageUrl
 ) {}
